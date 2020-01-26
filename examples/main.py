3ea794f08522448c118d654c4a14f1037fdf2d6f
up201806728@ms.uporto.pt <up201806728@ms.uporto.pt>
2020-01-26 11:31:12 +0000

    Organize files structure

--- examples/main.py
+++
@@ -1,7 +0,0 @@
-from examples.circle_example import *
-from examples.rect_example import *
-
-# execute only one of the functions rect_example or circle_example
-
-rect_example()
-# circle_example()
